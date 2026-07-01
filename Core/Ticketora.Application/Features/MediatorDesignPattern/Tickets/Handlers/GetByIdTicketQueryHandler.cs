c083dbc38e8865d929ee2a671b01a3eb6a7a9eed
MuratYucedag <yucedagmurat23@gmail.com>
2026-07-01 21:56:19 +0300

    ...

---
+++ Core/Ticketora.Application/Features/MediatorDesignPattern/Tickets/Handlers/GetByIdTicketQueryHandler.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ticketora.Application.Features.MediatorDesignPattern.Tickets.Queries;
+using Ticketora.Application.Features.MediatorDesignPattern.Tickets.Results;
+using Ticketora.Persistence.Context;
+
+namespace Ticketora.Application.Features.MediatorDesignPattern.Tickets.Handlers
+{
+    public class GetByIdTicketQueryHandler : IRequestHandler<GetByIdTicketQuery, GetByIdTicketQueryResult>
+    {
+        private readonly TicketoraContext _context;
+        public GetByIdTicketQueryHandler(TicketoraContext context)
+        {
+            _context = context;
+        }
+        public async Task<GetByIdTicketQueryResult> Handle(GetByIdTicketQuery request, CancellationToken cancellationToken)
+        {
+            var value = await _context.Tickets
+                .Where(x => x.TicketId == request.Id)
+                .Select(x => new GetByIdTicketQueryResult
+                {
+                    TicketId = x.TicketId,
+                    TicketNumber = x.TicketNumber,
+                    Price = x.Price,
+                    PurchaseDate = x.PurchaseDate,
+                    IsUsed = x.IsUsed
+                }).FirstOrDefaultAsync();
+
+            return value;
+        }
+    }
+}
---
+++ Core/Ticketora.Application/Features/MediatorDesignPattern/Tickets/Handlers/GetTicketQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ticketora.Application.Features.MediatorDesignPattern.Tickets.Queries;
+using Ticketora.Application.Features.MediatorDesignPattern.Tickets.Results;
+using Ticketora.Persistence.Context;
+
+namespace Ticketora.Application.Features.MediatorDesignPattern.Tickets.Handlers
+{
+    public class GetTicketQueryHandler : IRequestHandler<GetTicketQuery, List<GetTicketQueryResult>>
+    {
+        private readonly TicketoraContext _context;
+        public GetTicketQueryHandler(TicketoraContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<GetTicketQueryResult>> Handle(GetTicketQuery request, CancellationToken cancellationToken)
+        {
+            var values = await _context.Tickets
+                .Select(x => new GetTicketQueryResult
+                {
+                    TicketId = x.TicketId,
+                    TicketNumber = x.TicketNumber,
+                    Price = x.Price,
+                    PurchaseDate = x.PurchaseDate,
+                    IsUsed = x.IsUsed
+                }).ToListAsync();
+
+            return values;
+        }
+    }
+}
